6b1b63039cadf52b736eadfda10b6419d91bcefb
Clementine Bread <soyflourbread@gmail.com>
2024-04-27 15:22:47 +0800

    add stuff.

--- system.sh
+++ system.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-BOARDCONFIG="BoardConfig-SPI_NAND-Buildroot-RV1103_Luckfox_Pico_Mini_B-IPC.mk"
 ROOTFS_NAME="rootfs-alpine.tar.gz"
 
 rm -rf sdk/sysdrv/custom_rootfs/
@@ -14,9 +13,20 @@ source env_install_toolchain.sh
 popd || exit
 
 rm -rf .BoardConfig.mk
-cp "project/cfg/BoardConfig_IPC/$BOARDCONFIG" .BoardConfig.mk
+echo "6" | ./build.sh lunch
 echo "export RK_CUSTOM_ROOTFS=../sysdrv/custom_rootfs/$ROOTFS_NAME" >> .BoardConfig.mk
 
-./build.sh
+# build sysdrv - rootfs
+./build.sh uboot
+./build.sh kernel
+./build.sh env
+./build.sh app
+# package firmware
+./build.sh firmware
+./build.sh save
 
 popd || exit
+
+rm -rf output
+mkdir -p output
+cp sdk/output/image/update.img output/
