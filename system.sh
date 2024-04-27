f71d49e82794a8b034d43335df0c551fe0916b20
Clementine Bread <soyflourbread@gmail.com>
2024-04-27 16:40:56 +0800

    upgrade

--- system.sh
+++ system.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 ROOTFS_NAME="rootfs-alpine.tar.gz"
+DEVICE_NAME="pico-mini"
+
+while getopts ":f:d" opt; do
+  case ${opt} in
+    f) ROOTFS_NAME="${OPTARG}" ;;
+    d) DEVICE_NAME="${OPTARG}" ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
+
+DEVICE_ID="6"
+case $DEVICE_NAME in
+  pico-mini-b) DEVICE_ID="6" ;;
+  pico-plus) DEVICE_ID="7" ;;
+  pico-pro-max) DEVICE_ID="8" ;;
+  *)
+    echo "Invalid device: ${DEVICE_NAME}."
+    exit 1
+    ;;
+esac
 
 rm -rf sdk/sysdrv/custom_rootfs/
 mkdir -p sdk/sysdrv/custom_rootfs/
@@ -13,7 +36,7 @@ source env_install_toolchain.sh
 popd || exit
 
 rm -rf .BoardConfig.mk
-echo "6" | ./build.sh lunch
+echo "$DEVICE_ID" | ./build.sh lunch
 echo "export RK_CUSTOM_ROOTFS=../sysdrv/custom_rootfs/$ROOTFS_NAME" >> .BoardConfig.mk
 
 # build sysdrv - rootfs
@@ -29,4 +52,4 @@ popd || exit
 
 rm -rf output
 mkdir -p output
-cp sdk/output/image/update.img output/
+cp sdk/output/image/update.img "output/$DEVICE_NAME-sysupgrade.img"
