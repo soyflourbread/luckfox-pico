89783bb66abd8e7e67f27cb2fcc903507ad9237b
Clementine Bread <soyflourbread@gmail.com>
2024-05-05 14:35:49 +0800

    add rndis again?

--- rootfs.sh
+++ rootfs.sh
@@ -53,11 +53,11 @@ overlay() {
   echo "Include /etc/ssh/sshd_config.d/*.conf" >> \
     "$ROOTFS_WORKSPACE_MNT/etc/ssh/sshd_config"
 
-  ln -s "/etc/init.d/link_mount" \
-    "$ROOTFS_WORKSPACE_MNT/etc/runlevels/default/link_mount"
+  ln -s "/etc/init.d/00_link_mount" \
+    "$ROOTFS_WORKSPACE_MNT/etc/runlevels/default/00_link_mount"
 
-  # ln -s "/etc/init.d/usb_gadget" \
-  #   "$ROOTFS_WORKSPACE_MNT/etc/runlevels/default/usb_gadget"
+  ln -s "/etc/init.d/10_usb_gadget" \
+    "$ROOTFS_WORKSPACE_MNT/etc/runlevels/default/10_usb_gadget"
 }
 
 overlay
